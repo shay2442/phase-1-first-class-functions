3446bc1e358db51cd6318b37bd213ad53cf67770
Shay Wacker <shay2442@yahoo.com>
2021-08-11 15:30:40 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback) {
+    return callback()
+}
+
+function returnsANamedFunction() {
+    return function test() {
+
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+
+    }
+}
